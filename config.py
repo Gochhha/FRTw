f2d614923c847ead4207db633212d63928441e94
Gochhha <145044160+Gochhha@users.noreply.github.com>
2023-09-16 11:14:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 __version__ = '1.0.1'
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://root:secret_password@127.0.0.1/data')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://default:C5qV0oYezUSm@ep-blue-salad-48392986.us-east-1.postgres.vercel-storage.com:5432/verceldb')
